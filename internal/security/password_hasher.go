3671ae8e17fd52057e257e883a50574accb6c5b0
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-03-26 16:13:03 +0300

    security helpers

---
+++ internal/security/password_hasher.go
@@ -0,0 +1,34 @@
+package security
+
+import (
+	"golang.org/x/crypto/bcrypt"
+
+	"what-i-know-api/internal/usecase"
+)
+
+type BcryptPasswordHasher struct {
+	cost int
+}
+
+func NewBcryptPasswordHasher(cost int) *BcryptPasswordHasher {
+	if cost == 0 {
+		cost = bcrypt.DefaultCost
+	}
+
+	return &BcryptPasswordHasher{cost: cost}
+}
+
+func (h *BcryptPasswordHasher) Hash(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), h.cost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hashedPassword), nil
+}
+
+func (h *BcryptPasswordHasher) Compare(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+var _ usecase.PasswordHasher = (*BcryptPasswordHasher)(nil)
