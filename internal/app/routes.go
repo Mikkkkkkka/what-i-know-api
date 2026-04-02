40b01c6b208ce2e231f0b6965750717bbe2e22b5
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-04-02 05:17:24 +0300

    fix wrapped the httpApi to base path

--- internal/app/routes.go
+++ internal/app/routes.go
@@ -5,11 +5,11 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-
 	"github.com/mikkkkkkka/what-i-know-api/internal/api"
+	"github.com/mikkkkkkka/what-i-know-api/internal/config"
 )
 
-func SetupRouter(userHandler *api.UserHandler, noteHandler *api.NoteHandler, markHandler *api.MarkHandler) http.Handler {
+func SetupRouter(cfg config.Config, userHandler *api.UserHandler, noteHandler *api.NoteHandler, markHandler *api.MarkHandler) http.Handler {
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID)
@@ -18,28 +18,30 @@ func SetupRouter(userHandler *api.UserHandler, noteHandler *api.NoteHandler, mar
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Heartbeat("/health"))
 
-	r.Route("/users", func(r chi.Router) {
-		r.Post("/", userHandler.CreateUser)
-		r.Get("/{userID}", userHandler.GetUser)
-		r.Patch("/{userID}", userHandler.UpdateUser)
-		r.Delete("/{userID}", userHandler.DeleteUser)
+	r.Route(cfg.HTTPAPIBasePath, func(r chi.Router) {
+		r.Route("/users", func(r chi.Router) {
+			r.Post("/", userHandler.CreateUser)
+			r.Get("/{userID}", userHandler.GetUser)
+			r.Patch("/{userID}", userHandler.UpdateUser)
+			r.Delete("/{userID}", userHandler.DeleteUser)
 
-		r.Get("/{userID}/notes", noteHandler.ListNotesByUser)
-		r.Get("/{userID}/marks", markHandler.ListMarksByUser)
-	})
+			r.Get("/{userID}/notes", noteHandler.ListNotesByUser)
+			r.Get("/{userID}/marks", markHandler.ListMarksByUser)
+		})
 
-	r.Route("/notes", func(r chi.Router) {
-		r.Post("/", noteHandler.CreateNote)
-		r.Get("/{noteID}", noteHandler.GetNote)
-		r.Patch("/{noteID}", noteHandler.UpdateNote)
-		r.Delete("/{noteID}", noteHandler.DeleteNote)
-	})
+		r.Route("/notes", func(r chi.Router) {
+			r.Post("/", noteHandler.CreateNote)
+			r.Get("/{noteID}", noteHandler.GetNote)
+			r.Patch("/{noteID}", noteHandler.UpdateNote)
+			r.Delete("/{noteID}", noteHandler.DeleteNote)
+		})
 
-	r.Route("/marks", func(r chi.Router) {
-		r.Post("/", markHandler.CreateMark)
-		r.Get("/{markID}", markHandler.GetMark)
-		r.Patch("/{markID}", markHandler.UpdateMark)
-		r.Delete("/{markID}", markHandler.DeleteMark)
+		r.Route("/marks", func(r chi.Router) {
+			r.Post("/", markHandler.CreateMark)
+			r.Get("/{markID}", markHandler.GetMark)
+			r.Patch("/{markID}", markHandler.UpdateMark)
+			r.Delete("/{markID}", markHandler.DeleteMark)
+		})
 	})
 
 	return r
